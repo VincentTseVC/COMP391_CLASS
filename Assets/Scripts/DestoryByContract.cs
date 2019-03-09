6f2c1fb60a8dfa85ca914b7888960d684d195801
VincentTseVC <from_vincent@hotmail.com>
2019-03-08 21:13:08 -0500

    added explusion

---
+++ Assets/Scripts/DestoryByContract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryByContract : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boundary") return;
+
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            // show game over;
+        }
+
+
+        // Instantiate asteriod explosion animation
+        Instantiate(explosion, this.transform.position, this.transform.rotation);
+
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+}
